138597bf896272a2f68d2c15a0288d49689ec694
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2020-12-06 22:37:19 +0200

    Add brainGCD game, move locale to constants

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ export const getRandomInt = (min, max) => {
 export const greatPlayer = (playerName, TEXT_COLORS, locale) => {
   console.log(TEXT_COLORS.yellow, locale.greetings.text1);
   console.log(TEXT_COLORS.cyan, `${locale.greetings.text2}${playerName}${locale.greetings.text3}`);
-  console.log(TEXT_COLORS.cyan, locale.rules);
+};
+
+export const showRules = (TEXT_COLORS, locale) => {
+  console.log(TEXT_COLORS.cyan, locale);
 };
 
 export const checkAnswer = (answer, correctAnswer) => answer === correctAnswer;
