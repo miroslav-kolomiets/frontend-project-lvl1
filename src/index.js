78157f89982d01fbe9f27136fd446f784d4debc7
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2020-12-06 19:58:27 +0200

    Add BrainCalc game, move common logic to index

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import readlineSync from 'readline-sync';
+
+export const getPlayerName = (locale) => readlineSync.question(locale.questions.name);
+
+export const getRandomInt = (min, max) => {
+  const minimum = Math.ceil(min);
+  const maximum = Math.floor(max);
+  return Math.floor(Math.random() * (maximum - minimum)) + minimum;
+};
+
+export const greatPlayer = (playerName, TEXT_COLORS, locale) => {
+  console.log(TEXT_COLORS.yellow, locale.greetings.text1);
+  console.log(TEXT_COLORS.cyan, `${locale.greetings.text2}${playerName}${locale.greetings.text3}`);
+  console.log(TEXT_COLORS.cyan, locale.rules);
+};
+
+export const checkAnswer = (answer, correctAnswer) => answer === correctAnswer;
