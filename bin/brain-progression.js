d862311e7b3935b412cab436bd7b47db4a8a1b5b
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2020-12-25 14:58:10 +0200

    First review refactoring.

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { startGame } from '../src/index.js';
-import { QUESTIONS_NUMBER, RULES_PROGRESSION_GAME  } from "../src/constants.js";
+import { QUESTIONS_NUMBER, RULES_PROGRESSION_GAME } from "../src/constants.js";
 import brainProgressionGame from '../src/games/progression-game.js';
 
 startGame(brainProgressionGame(QUESTIONS_NUMBER), RULES_PROGRESSION_GAME);
