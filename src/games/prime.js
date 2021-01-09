00663ab31ed6448016e1de82136c17b00048dbb7
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2021-01-09 23:14:53 +0200

    Fix isPrime function&

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,6 +6,7 @@ const GAME_START = 1;
 const GAME_END = 100;
 
 const isPrime = (num) => {
+  if (num === 1) return false;
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
